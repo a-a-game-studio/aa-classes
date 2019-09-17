abbad8b9bcf4347d7373e9792a576b4a4d5498f2
elexfreeman <elextraza@gmail.com>
2019-09-17 21:18:19 +0400

    build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,5 @@
 import * as UserModule from './User/UserModule';
 import * as BaseModule from "./BaseClass/BaseModule";
-import * as SysteCoreModule from "./SystemCore/SysteCoreModule";
+import * as SysteCoreModule from "./SystemCore/SystemCoreModule";
 import * as WalletModule from "./Wallet/WalletModule";
 export { UserModule, BaseModule, SysteCoreModule, WalletModule, };
