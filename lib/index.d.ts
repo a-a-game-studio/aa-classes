71ac80302e814eb79af6f52537546d80d87f3706
elexfreeman <elextraza@gmail.com>
2019-09-20 17:26:43 +0400

    build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,4 +3,5 @@ import * as BaseModule from "./BaseClass/BaseModule";
 import * as SysteCoreModule from "./SystemCore/SystemCoreModule";
 import * as WalletModule from "./Wallet/WalletModule";
 import * as FileModule from "./File/FileModule";
-export { UserModule, BaseModule, SysteCoreModule, WalletModule, FileModule, };
+import * as Components from "@a-a-game-studio/aa-components/lib";
+export { UserModule, BaseModule, SysteCoreModule, WalletModule, FileModule, Components, };
