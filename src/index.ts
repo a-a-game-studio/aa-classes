cd1f63c8ca0214f97b9ddb924001c1be90d7682d
elexfreeman <elextraza@gmail.com>
2019-09-20 17:26:00 +0400

    Components
    вынес компонеты в index->export

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,14 @@ import * as BaseModule from "./BaseClass/BaseModule";
 import * as SysteCoreModule from "./SystemCore/SystemCoreModule";
 import * as WalletModule from "./Wallet/WalletModule";
 import * as FileModule from "./File/FileModule";
+import * as Components from "@a-a-game-studio/aa-components/lib";
+
+
 export {
     UserModule,
     BaseModule,
     SysteCoreModule,
     WalletModule,
     FileModule,
+    Components,
 }
