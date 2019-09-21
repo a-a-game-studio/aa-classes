6a6c1cf64d50a3570399293fae255d887e85ca64
elexfreeman <elextraza@gmail.com>
2019-09-22 00:22:17 +0400

    ListDBI

--- lib/BaseClass/BaseModule.d.ts
+++ lib/BaseClass/BaseModule.d.ts
@@ -1,5 +1,5 @@
 import { AABase } from './AABase';
 import { BaseActions } from './BaseActions';
 import { BaseDB } from './BaseDB';
-import { ListDB } from './ListDB';
-export { AABase, BaseActions, BaseDB, ListDB, };
+import { ListDB, ListDBI } from './ListDB';
+export { AABase, BaseActions, BaseDB, ListDB, ListDBI, };
--- src/BaseClass/BaseModule.ts
+++ src/BaseClass/BaseModule.ts
@@ -1,11 +1,12 @@
 import { AABase } from './AABase';
 import { BaseActions } from './BaseActions';
 import { BaseDB } from './BaseDB';
-import { ListDB } from './ListDB';
+import { ListDB, ListDBI } from './ListDB';
 
 export {
     AABase,
     BaseActions,
     BaseDB,
     ListDB,
+    ListDBI,
 }
